31bd8a33dfcc05a548e703b9960c1461db46f875
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2022-05-28 15:37:54 +0600

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -2,7 +2,7 @@ class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
         self.ListCommand = 's'
-        self.CloneCommand = 'c'
+        self.CloneCommand = 'd'
         self.CountCommand = 'co'
         self.CancelCommand = 'can'
         self.StatusCommand = 'status'
